2b9b513f539128aca78f6266a77ec4c4d8c5bcdc
yana999 <Abdramanova.yana@yandex.ru>
2021-12-09 12:45:23 +0300

    - 6 draft of task 2  - time?

--- src/main/java/ru/abdramanova/InnerJoin.java
+++ src/main/java/ru/abdramanova/InnerJoin.java
@@ -24,23 +24,29 @@ public class InnerJoin {
             Instant astart = Instant.now();
             List<Intersection> arrayListResult = arrayListJoin.innerJoin(new ArrayList<>(tableA), new ArrayList<>(tableB));
             Instant afinish = Instant.now();
-            System.out.println("Time of the ArrayList " + Duration.between(astart, afinish).toMillis());
+            System.out.println("Time of the ArrayList " + Duration.between(astart, afinish).toMillis() + " ms");
             //----------------LinkedList-----------------------------------------
             LinkedListJoin listJoin =  new LinkedListJoin();
+            LinkedList<Table> linkedTable1 =new LinkedList<>(tableA);
+            LinkedList<Table> linkedTable2 =new LinkedList<>(tableB);
+            linkedTable1.sort(Comparator.comparing(Table::getId));
+            linkedTable2.sort(Comparator.comparing(Table::getId));
+            tableB.sort(Comparator.comparing(Table::getId));
             Instant lstart = Instant.now();
-            List<Intersection> linkedListResult = listJoin.innerJoin(new LinkedList<>(tableA), new LinkedList<>(tableB));
+            List<Intersection> linkedListResult = listJoin.innerJoin(linkedTable1, linkedTable2);
             Instant lfinish = Instant.now();
-            System.out.println("Time of the sorted LinkedList " + Duration.between(lstart, lfinish).toMillis());
+            System.out.println("Time of the sorted LinkedList " + Duration.between(lstart, lfinish).toMillis()+ " ms");
             //------------------HashMap----------------------------------------
             Map<Long, List<Table>> table1 = tableA.stream().collect(Collectors.groupingBy(Table::getId));
             Map<Long, List<Table>> table2 = tableB.stream().collect(Collectors.groupingBy(Table::getId));
             HashMapJoin hashMapJoin = new HashMapJoin();
-            Instant hstart = Instant.now();
+            Instant hashStart = Instant.now();
+            long htstart = System.nanoTime();
             List<Intersection> hashMapResult = hashMapJoin.innerJoin(new HashMap<>(table1), new HashMap<>(table2));
-            Instant hfinish = Instant.now();
-            System.out.println("Time of the HashMap " + Duration.between(hstart, hfinish).toNanos());
-            System.out.println(hstart.getNano());
-            System.out.println(hfinish.getNano());
+            long htfinish = System.nanoTime();
+            Instant hashFinish = Instant.now();
+            System.out.println("Time of the hashMap  " + Duration.between(hashStart, hashFinish).toMillis()+ " ms");
+            System.out.println( Math.ceil((htfinish - htstart)/1000000.0) + " ms");
             ListWriter writer = new ListWriter();
             if(args.length == 2){
                 writer.saveTable(args[0], "ArrayListResult.txt", arrayListResult);
--- src/main/java/ru/abdramanova/join/LinkedListJoin.java
+++ src/main/java/ru/abdramanova/join/LinkedListJoin.java
@@ -12,8 +12,6 @@ public class LinkedListJoin implements InnerJoin<LinkedList<Table>>{
     @Override
     public List<Intersection> innerJoin(LinkedList<Table> table1, LinkedList<Table> table2) {
         List<Intersection> result = new LinkedList<>();
-        table1.sort(Comparator.comparing(Table::getId));
-        table2.sort(Comparator.comparing(Table::getId));
         for(int i = 0, j = 0; i < table1.size() && j < table2.size();){
             if(table1.get(i).getId() == table2.get(j).getId()) {
                 for(int k = j; k < table2.size() && table1.get(i).getId() == table2.get(k).getId(); ++k){
@@ -28,16 +26,4 @@ public class LinkedListJoin implements InnerJoin<LinkedList<Table>>{
         }
         return result;
     }
-
-    /*while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] == nums2[j]) {
-                result.add(nums1[i]);
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]){
-                i++;
-            } else {
-                j++;
-            }
-        }*/
 }
